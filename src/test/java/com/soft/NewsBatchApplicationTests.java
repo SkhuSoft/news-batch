d2f42e4ffad0605d15afca877f799eb51529d58e
BoBinLee <cultist_tp@naver.com>
2015-03-31 09:11:58 +0900

    initialize batch commit

---
+++ src/test/java/com/soft/NewsBatchApplicationTests.java
@@ -0,0 +1,19 @@
+package com.soft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.soft.NewsBatchApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = NewsBatchApplication.class)
+public class NewsBatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		int num;
+	}
+
+}
